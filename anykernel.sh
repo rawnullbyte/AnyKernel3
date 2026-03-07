056ced0c8788ea8905cbe73abbe72e5b7f60d910
rawnullbyte <120054040+rawnullbyte@users.noreply.github.com>
2026-03-07 20:28:22 +0100

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,40 +1,16 @@
 ### AnyKernel3 Ramdisk Mod Script
 ## osm0sis @ xda-developers
 
-### AnyKernel setup
-# global properties
 properties() { '
-kernel.string=Xine Kernel by NSR-XINE
+kernel.string=MeowKernel by meowrah
 do.devicecheck=1
-do.modules=1
-do.systemless=1
+do.modules=0
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=lisa
-device.name2=
-device.name3=
-device.name4=
-device.name5=
-supported.versions=15.0-16.0
-supported.patchlevels=
-'; } # end properties
-
-### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
-
-# begin passthrough patch
-passthrough() {
-if [ ! "$(getprop persist.sys.fuse.passthrough.enable)" ]; then
-	ui_print "Remounting /system as rw..."
-	$home/tools/busybox mount -o rw,remount /system
-	ui_print "Patching system's build prop for FUSE Passthrough..."
-	patch_prop /system/build.prop "persist.sys.fuse.passthrough.enable" "true"
-fi
-} # end passthrough patch
+supported.versions=15 - 16
+'; }
 
 ## boot shell variables
 BLOCK=boot;
@@ -42,72 +18,13 @@ IS_SLOT_DEVICE=1;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh && passthrough;
+# import functions/variables
+. tools/ak3-core.sh;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-## end boot install
-
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
-
-# init_boot shell variables
-#BLOCK=init_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for init_boot patching
-#reset_ak;
-
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
-
-#write_boot;
-## end init_boot install
-
-## vendor_kernel_boot shell variables
-#BLOCK=vendor_kernel_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_kernel_boot patching
-#reset_ak;
-
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
-
-#flash_boot;
-## end vendor_kernel_boot install
-
-## vendor_boot files attributes
-vendor_boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-}
-# end attributes
-
-## vendor_boot shell variables
-BLOCK=vendor_boot;
-IS_SLOT_DEVICE=1;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_boot patching
-reset_ak;
-
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+## boot install
+# We use split/flash to swap the Image without touching the ramdisk
 split_boot;
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 flash_boot;
 
-## end vendor_boot install
+# uncomment if you have a custom dtbo.img in the zip root
+# flash_dtbo;
